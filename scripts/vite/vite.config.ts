adc793439bb22125857b0b76e562ca6d587967c5
Ilya 'Alaie' Shafeev <ilya.tanc@gmail.com>
2023-05-16 06:00:50 -0700

    Fix...

--- scripts/vite/vite.config.ts
+++ scripts/vite/vite.config.ts
@@ -2,22 +2,31 @@ import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 import svgr from "vite-plugin-svgr";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import { readdirSync } from "fs";
+import { readdirSync, existsSync } from "fs";
 import path from "path";
 
 import type { UserConfig } from "vite";
 
 function getChallengesAssets(source: string) {
-  return readdirSync(source, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => ({
-      src: [
-        path.resolve(__dirname, `/src/challenges/${dirent.name}/public/fonts`),
-        path.resolve(__dirname, `/src/challenges/${dirent.name}/public/images`),
-        path.resolve(__dirname, `/src/challenges/${dirent.name}/public/data`)
-      ],
-      dest: ""
-    }));
+  if (existsSync(source))
+    return readdirSync(source, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => ({
+        src: [
+          path.resolve(
+            __dirname,
+            `/src/challenges/${dirent.name}/public/fonts`
+          ),
+          path.resolve(
+            __dirname,
+            `/src/challenges/${dirent.name}/public/images`
+          ),
+          path.resolve(__dirname, `/src/challenges/${dirent.name}/public/data`)
+        ],
+        dest: ""
+      }));
+
+  return [];
 }
 
 const config: UserConfig = {
