722d26ea6b86b6e58695e237dda36a0a9bb71553
Ilya 'Alaie' Shafeev <ilya.tanc@gmail.com>
2023-05-16 07:29:15 -0700

    Add page resultsSummaryComponent

---
+++ src/pages/resultsSummaryComponent.page.tsx
@@ -0,0 +1,10 @@
+import Page from "../challenges/resultsSummaryComponent/src/app";
+
+export { Page };
+
+export const documentProps = {
+  title: "Results Summary Component",
+  description: "Results Summary Component, Front-end Mentor challenge.",
+  fonts:
+    "https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;700&display=swap"
+};
