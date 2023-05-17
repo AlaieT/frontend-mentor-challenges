7e43cc4274b3ed6719ea6775d672b73c73aa4cb9
Ilya 'Alaie' Shafeev <ilya.tanc@gmail.com>
2023-05-17 02:04:31 -0700

    Add page launch-countdown-timer

---
+++ src/pages/launchCountdownTimer.page.tsx
@@ -0,0 +1,10 @@
+import Page from "../challenges/launchCountdownTimer/src/app";
+
+export { Page };
+
+export const documentProps = {
+  title: "Launch Countdown Timer",
+  description: "Launch Countdown Timer, Front-end Mentor challenge.",
+  fonts:
+    "https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@700&display=swap"
+};
