9fc785093576e192ebdd8e024e1e0593e696f782
Ilya 'Alaie' Shafeev <ilya.tanc@gmail.com>
2023-05-17 01:47:11 -0700

    Add multi-step-form page

---
+++ src/pages/multiStepForm.page.tsx
@@ -0,0 +1,10 @@
+import Page from "../challenges/multiStepForm/src/app";
+
+export { Page };
+
+export const documentProps = {
+  title: "Multi Step Form",
+  description: "Multi Step Form, Front-end Mentor challenge.",
+  fonts:
+    "https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
+};
